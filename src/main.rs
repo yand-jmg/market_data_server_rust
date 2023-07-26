3b4fa58684dc6caf72395145fd7ecf1ac4f26c29
Jason McGuiness <coder@hussar.me.uk>
2023-07-26 17:36:17 +0100

    Added CTRL-C handling, I think...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use signal_hook::{consts::SIGINT, consts::SIGTERM};
 use simple_logger::SimpleLogger;
+use std::{error::Error};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use tungstenite::connect;
 use url::Url;
 
@@ -7,8 +11,10 @@ static BINANCE_WS_API: &str = "wss://stream.binance.com:9443";
 static BINANCE_INSTRUMENT: &str = "ethbtc";
 static BINANCE_INSTRUMENT_DETAILS: &str = "depth5@100ms";
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     SimpleLogger::new().init().unwrap();
+    let exit_main = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(SIGTERM|SIGINT, Arc::clone(&exit_main))?;
     // TODO consider cmmand-line arguments for passing in the exchange URLs. Also some form of help.
     // TODO Need to handle all exchanges...
     let binance_url = format!("{}/ws/{}@{}", BINANCE_WS_API, BINANCE_INSTRUMENT, BINANCE_INSTRUMENT_DETAILS);
@@ -21,20 +27,22 @@ fn main() {
         log::info!("- {}: {:?}", header, header_value);
     }
     log::info!("Commencing receiving market-data.");
-    loop {
-        let msg = socket.read_message().expect(&format!("Error reading message from: '{}'.", binance_url));
-        let msg = match msg {
+    while !exit_main.load(Ordering::Relaxed) {
+        let raw_msg = socket.read_message().expect(&format!("Error reading message from: '{}'.", binance_url));
+        let msg = match raw_msg {
             tungstenite::Message::Text(s) => s,
             _ => {
-                // TODO need to just log this as an error, possibly re-connect....
-                panic!("Error getting text from: '{}'.", binance_url);
+                // TODO need to just log this as an error, possibly re-connect...
+                log::warn!("Error getting text from: '{}'.", binance_url);
+                // TODO need to try re-connecting?
+                break;
             }
         };
 
         let parsed_data: serde_json::Value = serde_json::from_str(&msg).expect(&format!("Unable to parse message from: '{}'.", binance_url));
         log::info!("{:?}", parsed_data);
-        // TODO Need to push this to gRPC
+        // TODO Need to push this to gRPC...
     }
-    // TODO need CTRL-C handler.
     log::info!("Finished receiving market-data, exiting.");
+    Ok(())
 }
